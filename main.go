b1a0f3ef30817cc1e929fc1dbc3e7da55d4508db
davidw1457 <89819150+davidw1457@users.noreply.github.com>
2024-11-12 16:12:30 -0600

    preliminary ui work

--- main.go
+++ main.go
@@ -2,21 +2,47 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	myApp := app.New()
-	myWindow := myApp.NewWindow("Hello")
-	myWindow.SetContent(widget.NewLabel("Hello"))
+	myWindow := myApp.NewWindow("DCUI Scraper")
 
-	myWindow.Show()
-	myApp.Run()
-	tidyUp()
+	updateButton := widget.NewButton("Update DCUI Database", updateDatabase)
+	filterText := canvas.NewText("Filters", color.White)
+	titleFilterButton := widget.NewButton("Title", titleFilter)
+	dateFilterButton := widget.NewButton("Date Range", dateFilter)
+
+	leftPane := container.New(layout.NewVBoxLayout(), updateButton, filterText, titleFilterButton, dateFilterButton)
+	// TODO: Add a widget.NewList to hold filter contents 
+	centerPane := container.New(layout.NewVBoxLayout(), canvas.NewText("Filter Options:", color.White))
+	// TODO: Add a widget.NewTable to hold filter output
+	rightPane := container.New(layout.NewVBoxLayout(), canvas.NewText("Filter Output:", color.White))
+
+	myWindow.SetContent(container.New(layout.NewHBoxLayout(), leftPane, widget.NewSeparator(), centerPane, widget.NewSeparator(), rightPane, layout.NewSpacer()))
+
+	myWindow.ShowAndRun()
 }
 
-func tidyUp() {
-	fmt.Println("Exited")
+func updateDatabase() {
+	fmt.Println("updateButton pressed")
+}
+
+func titleFilter() {
+	fmt.Println("titleFilterButton pressed")
+}
+
+func dateFilter() {
+	fmt.Println("dateFilter pressed")
+}
+
+func buildContent() {
+	
 }
